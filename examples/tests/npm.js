26939c7fda89985526d9bfe31148e50082b80b2c
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2014-12-10 07:39:26 +0900

    Change the demo scenario

---
+++ examples/tests/npm.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "Demo test Npmjs" : function (browser) {
+    browser
+      .url("https://www.npmjs.com")
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=search]', 'nightwatch')
+      .waitForElementVisible('input[type=submit]', 1000)
+      .click('input[type=submit]')
+      .pause(1000)
+      .assert.elementPresent('a[href="/packages/nightwatch"]', 'The Night Watch')
+      .end(); 
+  }
+};
